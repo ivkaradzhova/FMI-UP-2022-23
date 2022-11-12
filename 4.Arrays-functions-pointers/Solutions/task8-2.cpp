2e260b06403686cd6bd009e44794d1fbd320d940
Ivaylo-Valentinov <58509466+Ivaylo-Valentinov@users.noreply.github.com>
2022-11-12 17:44:55 +0200

    Add solutions to task8

---
+++ 4.Arrays-functions-pointers/Solutions/task8-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+float division(float num, float den)
+{
+    if (den == 0) {
+        throw std::exception("Math error: Attempted to divide by Zero\n");
+    }
+
+    return (num / den);
+
+}
+
+int main()
+{
+    float numerator, denominator, result;
+    numerator = 12.5;
+    denominator = 0;
+
+    try {
+        result = division(numerator, denominator);
+
+        std::cout << "The quotient is "
+            << result << std::endl;
+    }
+
+    catch (const std::exception& e) {
+        std::cout << "Exception occurred" << std::endl
+            << e.what() << std::endl;
+    }
+    return 0;
+}
