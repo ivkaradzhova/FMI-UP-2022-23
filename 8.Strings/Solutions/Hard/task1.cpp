c49bb1af8f4d291b5eb2435b5e64c1c35bf177df
karadzhovai <karadzhovai@vmware.com>
2022-12-18 22:11:58 +0200

    add finished task1

--- 8.Strings/Solutions/Hard/task1.cpp
+++ 8.Strings/Solutions/Hard/task1.cpp
@@ -5,6 +5,34 @@ const int MAX_SIZE = 1024;
 const char FILE_SEPERATOR = '/';
 const char DOT = '.'; 
 
+bool validate_path(char file_path[]) {
+    int length = strlen(file_path);
+
+    // check for dir1/dir2
+    if (length > 0 && file_path[0] != FILE_SEPERATOR) {
+        std::cout << "File path " << file_path << " doesn't start with /" << std::endl;
+        return false;
+    }
+
+    //check for /dir1//dir2
+    for (int i = 1; i < length; i++) {
+        if ((file_path[i] == FILE_SEPERATOR) && file_path[i - 1] == FILE_SEPERATOR) {
+            std::cout << "File path " << file_path << " contains empty directory names" << std::endl;
+            return false;
+        }
+    }
+    
+    //check for /dir1/file1.txt/dir2
+    char* dot_occurence = strchr(file_path, DOT);
+
+    if (dot_occurence != nullptr && strchr(dot_occurence, FILE_SEPERATOR) != nullptr) {
+        std::cout << "File path " << file_path << " contains empty a file with a subdirectory" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
 bool array_contains_string(char arr[][MAX_SIZE], int size, char string[]) {
     for (int i = 0; i < size; i++) {
         if (strcmp(arr[i], string) == 0) {
@@ -18,6 +46,13 @@ bool is_file(char file_path[]) {
     return strchr(file_path, DOT) != nullptr;
 }
 
+void add_directory(char file_path[], char dirs_list[][MAX_SIZE], int& number_of_dirs) {
+    if(!is_file(file_path) && !array_contains_string(dirs_list, number_of_dirs, file_path)) {
+        strcpy(dirs_list[number_of_dirs], file_path);
+        number_of_dirs++;
+    }
+}
+
 void separate_directories(char file_path[], char dirs_in_file_path[][MAX_SIZE], int& number_of_dirs) {
     int curr_index = 0;
 
@@ -26,57 +61,35 @@ void separate_directories(char file_path[], char dirs_in_file_path[][MAX_SIZE], 
             // add the newly discovered path to the list of directories - dirs_in_file_path,
             // if it is not there already
             char curr_path[MAX_SIZE] = "";
-            strncpy(curr_path, file_path, curr_index + 1);
-            
-            if(!is_file(curr_path) && !array_contains_string(dirs_in_file_path, number_of_dirs, curr_path)) {
-                strcpy(dirs_in_file_path[number_of_dirs], curr_path);
-                number_of_dirs++;
-            }
+            strncpy(curr_path, file_path, curr_index);
+            add_directory(curr_path, dirs_in_file_path, number_of_dirs);
         }
         curr_index++;
     }
-}
 
-
-bool validate_path(char file_path[]) {
-    // check for dir1/dir2
-    if (strlen(file_path) > 0 && file_path[0] != FILE_SEPERATOR) {
-        std::cout << "File path " << file_path << " doesn't start with /" << std::endl;
-        return false;
+    // add the path to the last directory (after the last '/')
+    if (file_path[curr_index - 1] != '/') {
+        char curr_path[MAX_SIZE] = "";
+        strcpy(curr_path, file_path);
+        add_directory(curr_path, dirs_in_file_path, number_of_dirs);
     }
 
-    //check for /dir1//dir2
-    for (int i = 1; i < strlen(file_path); i++) {
-        if (file_path[i] == FILE_SEPERATOR && file_path[i - 1] == FILE_SEPERATOR) {
-            std::cout << "File path " << file_path << " contains empty directory names" << std::endl;
-            return false;
-        }
-    }
-
-    //check for /dir1/file1.txt/dir2
-    char* dot_occurence = strchr(file_path, DOT);
-    if (dot_occurence != nullptr && strchr(dot_occurence, FILE_SEPERATOR)) {
-        std::cout << "File path " << file_path << " contains empty a file with a subdirectory" << std::endl;
-        return false;
-    }
-
-    return true;
 }
 
 int main() {
     int n;
     std::cin >> n;
     std::cin.ignore();
-    assert(n < 1024);
+    assert(n < MAX_SIZE);
 
     char arr[MAX_SIZE][MAX_SIZE], all_dirs[MAX_SIZE][MAX_SIZE];
     int num_all_dirs = 0;
-    
     for (int i = 0; i < n; i++) {
         std::cin.getline(arr[i], MAX_SIZE);
-        if (validate_path(arr[i])) {
+        
+        if (!validate_path(arr[i])) {
             std::cout << "Validation failed" << std::endl;
-            return 0;
+            return 1;
         }
     }
 
